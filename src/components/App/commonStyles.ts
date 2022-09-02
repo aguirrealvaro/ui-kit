a4d1993527646c554eaf7f0a48d1debf6209ca22
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2022-09-02 19:40:14 -0300

    Remove unnecessary style

--- src/components/App/commonStyles.ts
+++ src/components/App/commonStyles.ts
@@ -1,19 +1,7 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 
 export const Wrapper = styled.div`
   max-width: 1200px;
   width: 90%;
   margin: 0 auto;
-`;
-
-export const originalImageWitdth = css`
-  > span {
-    position: unset !important;
-  }
-  img {
-    object-fit: contain;
-    width: 100% !important;
-    position: relative !important;
-    height: unset !important;
-  }
 `;
