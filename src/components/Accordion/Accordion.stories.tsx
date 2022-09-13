141623b0cede1054fd48585e586a31cba9c8ce9e
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2022-09-13 18:52:11 -0300

    Improve story

--- src/components/Accordion/Accordion.stories.tsx
+++ src/components/Accordion/Accordion.stories.tsx
@@ -7,22 +7,8 @@ export default {
 } as ComponentMeta<typeof Accordion>;
 
 export const Primary: ComponentStory<typeof Accordion> = () => {
-  const content = (
-    <>
-      content
-      <br />
-      content
-      <br />
-      content
-      <br />
-      content
-      <br />
-      content
-      <br />
-      content
-      <br />
-    </>
-  );
+  const content =
+    "Lorem ipsum dolor sit ametLorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat maxime corporis ullam voluptate quasi amet accusamus quae at laborum! Autem voluptas dolorum libero laboriosam praesentium ipsa quia vero odio quidem.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat maxime corporis ullam voluptate quasi amet accusamus quae at laborum! Autem voluptas dolorum libero laboriosam praesentium ipsa quia vero odio quidem.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat maxime corporis ullam voluptate quasi amet accusamus quae at laborum! Autem voluptas dolorum libero laboriosam praesentium ipsa quia vero odio quidem.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat maxime corporis ullam voluptate quasi amet accusamus quae at laborum! Autem voluptas dolorum libero laboriosam praesentium ipsa quia vero odio quidem.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat maxime corporis ullam voluptate quasi amet accusamus quae at laborum! Autem voluptas dolorum libero laboriosam praesentium ipsa quia vero odio quidem.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat maxime corporis ullam voluptate quasi amet accusamus quae at laborum! Autem voluptas dolorum libero laboriosam praesentium ipsa quia vero odio quidem., consectetur adipisicing elit. Placeat maxime corporis ullam voluptate quasi amet accusamus quae at laborum! Autem voluptas dolorum libero laboriosam praesentium ipsa quia vero odio quidem.";
 
-  return <Accordion title={"Click me"} content={content} />;
+  return <Accordion title="Click me" content={content} />;
 };
