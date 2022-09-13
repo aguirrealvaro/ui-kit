bc9e25b96c82e1c2334eb39b74c5f6a4c66985aa
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2022-09-13 16:51:29 -0300

    Finish Breadcrumbs

--- src/components/Breadcrumbs/Breadcrumbs.tsx
+++ src/components/Breadcrumbs/Breadcrumbs.tsx
@@ -1,4 +1,5 @@
-import { FunctionComponent, ReactNode } from "react";
+import { Children, FunctionComponent, ReactNode } from "react";
+import styled from "styled-components";
 
 type BreadcrumbsProps = {
   children: ReactNode;
@@ -7,7 +8,31 @@ type BreadcrumbsProps = {
 export const Breadcrumbs: FunctionComponent<BreadcrumbsProps> = ({ children }) => {
   return (
     <nav>
-      <ol>{children}</ol>
+      <List>
+        {Children.map(children, (child, index) => {
+          const count = Children.count(children);
+          const showSeparator = index !== count - 1;
+
+          return (
+            <ItemList>
+              <span>{child}</span>
+              {showSeparator && <Separator>/</Separator>}
+            </ItemList>
+          );
+        })}
+      </List>
     </nav>
   );
 };
+
+const List = styled.ol`
+  display: flex;
+`;
+
+const ItemList = styled.li`
+  list-style: none;
+`;
+
+const Separator = styled.span`
+  margin: 0 0.5rem;
+`;
