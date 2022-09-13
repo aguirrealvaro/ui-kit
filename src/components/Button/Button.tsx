52c384bf8d73ce8d1b45367c211c855d572adb02
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2022-09-13 18:00:46 -0300

    Clean button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,6 @@
 import { FunctionComponent, ButtonHTMLAttributes } from "react";
 import styled, { css, FlattenSimpleInterpolation } from "styled-components";
+import { theme } from "../App";
 import { Spinner } from "../Spinner";
 
 const ANIMATION_TIME = 300;
@@ -51,37 +52,6 @@ export const Button: FunctionComponent<
   );
 };
 
-const getShapeStyles = (shape: ShapeType): FlattenSimpleInterpolation => {
-  const shapeOptions = {
-    default: css`
-      border-radius: 4px;
-    `,
-    pill: css`
-      border-radius: 42px;
-    `,
-    circle: css`
-      border-radius: 100%;
-      width: 56px;
-      height: 56px;
-      padding: 0;
-      text-align: center;
-    `,
-    rectangle: css`
-      border-radius: 0px;
-    `,
-  };
-
-  return shapeOptions[shape];
-};
-
-const variantColors: Record<VariantType, string> = {
-  default: "blue",
-  positive: "green",
-  negative: "red",
-  warning: "yellow",
-  neutral: "black",
-};
-
 const getSizeStyles = (size: SizeType): FlattenSimpleInterpolation => {
   const sizeOptions: Record<SizeType, FlattenSimpleInterpolation> = {
     mini: css`
@@ -109,6 +79,93 @@ const getSizeStyles = (size: SizeType): FlattenSimpleInterpolation => {
   return sizeOptions[size];
 };
 
+const getShapeStyles = (shape: ShapeType): FlattenSimpleInterpolation => {
+  const shapeOptions = {
+    default: css`
+      border-radius: 4px;
+    `,
+    pill: css`
+      border-radius: 42px;
+    `,
+    circle: css`
+      border-radius: 100%;
+      width: 56px;
+      height: 56px;
+      padding: 0;
+      text-align: center;
+    `,
+    rectangle: css`
+      border-radius: 0px;
+    `,
+  };
+
+  return shapeOptions[shape];
+};
+
+const getColorStyles = (variant: VariantType, kind: KindType): FlattenSimpleInterpolation => {
+  const variantContainedOptions: Record<VariantType, FlattenSimpleInterpolation> = {
+    default: css`
+      background-color: ${theme.colors.blue};
+      color: ${theme.colors.white};
+      border: 1px solid ${theme.colors.blue};
+    `,
+    positive: css`
+      background-color: ${theme.colors.green};
+      color: ${theme.colors.white};
+      border: 1px solid ${theme.colors.green};
+    `,
+    warning: css`
+      background-color: ${theme.colors.yellow};
+      color: ${theme.colors.white};
+      border: 1px solid ${theme.colors.yellow};
+    `,
+    negative: css`
+      background-color: ${theme.colors.red};
+      color: ${theme.colors.white};
+      border: 1px solid ${theme.colors.red};
+    `,
+    neutral: css`
+      background-color: ${theme.colors.black};
+      color: ${theme.colors.white};
+      border: 1px solid ${theme.colors.black};
+    `,
+  };
+
+  const variantOutlinedOptions: Record<VariantType, FlattenSimpleInterpolation> = {
+    default: css`
+      background-color: ${theme.colors.white};
+      color: ${theme.colors.blue};
+      border: 1px solid ${theme.colors.blue};
+    `,
+    positive: css`
+      background-color: ${theme.colors.white};
+      color: ${theme.colors.green};
+      border: 1px solid ${theme.colors.green};
+    `,
+    warning: css`
+      background-color: ${theme.colors.white};
+      color: ${theme.colors.yellow};
+      border: 1px solid ${theme.colors.yellow};
+    `,
+    negative: css`
+      background-color: ${theme.colors.white};
+      color: ${theme.colors.red};
+      border: 1px solid ${theme.colors.red};
+    `,
+    neutral: css`
+      background-color: ${theme.colors.white};
+      color: ${theme.colors.black};
+      border: 1px solid ${theme.colors.black};
+    `,
+  };
+
+  if (kind === "contained") {
+    return variantContainedOptions[variant];
+  } else {
+    return variantOutlinedOptions[variant];
+  }
+};
+
 const CustomButton = styled.button<{
   block?: boolean;
   kind: KindType;
@@ -116,30 +173,11 @@ const CustomButton = styled.button<{
   variant: VariantType;
   shape: ShapeType;
 }>`
-  transition: all ${ANIMATION_TIME}ms ease;
   width: ${({ block }) => (block ? "100%" : "auto")};
   ${({ size }) => getSizeStyles(size)};
   ${({ shape }) => getShapeStyles(shape)};
-  ${({ kind, theme, variant }) => {
-    if (kind === "contained") {
-      return css`
-        background-color: ${theme.colors[variantColors[variant]]};
-        color: ${theme.colors.white};
-        border: 1px solid ${theme.colors[variantColors[variant]]};
-      `;
-    }
-    if (kind === "outlined") {
-      return css`
-        background-color: ${theme.colors.white};
-        color: ${theme.colors[variantColors[variant]]};
-        border: 1px solid ${theme.colors[variantColors[variant]]};
-        &:hover {
-          background-color: ${theme.colors[variantColors[variant]]};
-          color: ${theme.colors.white};
-        }
-      `;
-    }
-  }}
+  ${({ variant, kind }) => getColorStyles(variant, kind)};
+  transition: all ${ANIMATION_TIME}ms ease;
   &:disabled {
     background: #e9e9e9;
     border: 1px solid transparent;
