fa56739171481fe2b17649b0d17e299299177698
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2022-09-14 12:30:05 -0300

    Clean useLocalStorage

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -4,21 +4,14 @@ type UseLocalStorageReturn<T> = [T, (value: T) => void];
 
 export const useLocalStorage = <T>(key: string, initialValue: T): UseLocalStorageReturn<T> => {
   const [storedValue, setStoredValue] = useState(() => {
-    try {
-      const item = window.localStorage.getItem(key);
-      return item ? JSON.parse(item) : initialValue;
-    } catch (error) {
-      return initialValue;
-    }
+    const item = window.localStorage.getItem(key);
+    return item ? JSON.parse(item) : initialValue;
   });
 
   const setValue = (value: T) => {
-    try {
-      setStoredValue(value);
-      window.localStorage.setItem(key, JSON.stringify(value));
-    } catch (error) {
-      console.log(error);
-    }
+    setStoredValue(value);
+    window.localStorage.setItem(key, JSON.stringify(value));
   };
+
   return [storedValue, setValue];
 };
