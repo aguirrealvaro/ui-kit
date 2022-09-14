36c455107048fdffb46efe8ad79274e9d05c249a
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2022-09-14 13:44:01 -0300

    Add showBorder to accordion

--- src/components/Accordion/Accordion.stories.tsx
+++ src/components/Accordion/Accordion.stories.tsx
@@ -9,7 +9,7 @@ export default {
 export const Primary: ComponentStory<typeof Accordion> = () => {
   return (
     <div>
-      <Accordion title="Click me 1">
+      <Accordion title="Click me 1" showBorder>
         Lorem ipsum dolor sit ametLorem ipsum dolor sit amet, consectetur adipisicing elit.
         Placeat maxime corporis ullam voluptate quasi amet accusamus quae at laborum! Autem
         voluptas dolorum libero laboriosam praesentium ipsa quia vero odio quidem.Lorem ipsum
@@ -18,7 +18,7 @@ export const Primary: ComponentStory<typeof Accordion> = () => {
         praesentium ipsa quia vero odio quidem.Lorem ipsum dolor sit amet, consectetur
         adipisicing elit. Placeat maxime corporis ullam voluptate quasi amet accusamus quae at
       </Accordion>
-      <Accordion title="Click me 2">
+      <Accordion title="Click me 2" showBorder>
         Lorem ipsum dolor sit ametLorem ipsum dolor sit amet, consectetur adipisicing elit.
         Placeat maxime corporis ullam voluptate quasi amet accusamus quae at laborum! Autem
         voluptas dolorum libero laboriosam praesentium ipsa quia vero odio quidem.Lorem ipsum
@@ -27,7 +27,7 @@ export const Primary: ComponentStory<typeof Accordion> = () => {
         praesentium ipsa quia vero odio quidem.Lorem ipsum dolor sit amet, consectetur
         adipisicing elit. Placeat maxime corporis ullam voluptate quasi amet accusamus quae at
       </Accordion>
-      <Accordion title="Click me 3">
+      <Accordion title="Click me 3" showBorder>
         Lorem ipsum dolor sit ametLorem ipsum dolor sit amet, consectetur adipisicing elit.
         Placeat maxime corporis ullam voluptate quasi amet accusamus quae at laborum! Autem
         voluptas dolorum libero laboriosam praesentium ipsa quia vero odio quidem.Lorem ipsum
