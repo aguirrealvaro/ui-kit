ab5da9d0f91fd05ce6d4d25b3add0c34a2e2ae69
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2022-09-15 13:29:27 -0300

    1

--- src/components/Toast/index.ts
+++ src/components/Toast/index.ts
@@ -1,5 +1,4 @@
 export { Toast } from "./Toast";
 export { ToastContext, ToastProvider } from "./ToastProvider";
-export type { ToastContextType } from "./ToastProvider";
 export { useToast } from "./Toast.hooks";
 export type { ToastProps, ToastOptions } from "./Toast.types";
