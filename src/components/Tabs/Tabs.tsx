a8c9e10223288d1a260a47fb3cb9f0769cbf3dd3
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2022-09-15 21:57:55 -0300

    Improve tab

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,12 +1,12 @@
 import { FunctionComponent, Children, ReactNode, useState, isValidElement } from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 type TabsProps = {
   children: ReactNode;
 };
 
 export const Tabs: FunctionComponent<TabsProps> = ({ children }) => {
-  const [activeKey, setActiveKey] = useState<number>(1);
+  const [activeKey, setActiveKey] = useState<number>(0);
 
   const handleClickTab = (tab: number) => {
     setActiveKey(tab);
@@ -18,15 +18,23 @@ export const Tabs: FunctionComponent<TabsProps> = ({ children }) => {
         {Children.map(children, (child, index) => {
           if (!isValidElement(child)) return;
           const { title } = child.props as TabProps;
-          return <TabItem onClick={() => handleClickTab(index + 1)}>{title}</TabItem>;
+          return (
+            <TabItem
+              role="tab"
+              active={activeKey === index}
+              onClick={() => handleClickTab(index)}
+            >
+              {title}
+            </TabItem>
+          );
         })}
       </TabList>
       <div>
         {Children.map(children, (child, index) => {
           if (!isValidElement(child)) return;
           const { children } = child.props as TabProps;
-          if (activeKey !== index + 1) return null;
-          return <div>{children}</div>;
+          if (activeKey !== index) return null;
+          return <div role="tabpanel">{children}</div>;
         })}
       </div>
     </div>
@@ -44,8 +52,24 @@ export const Tab: FunctionComponent<TabProps> = ({ children }) => {
 
 const TabList = styled.div`
   display: flex;
+  margin-bottom: 1rem;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.12);
 `;
 
-const TabItem = styled.div`
+const TabItem = styled.div<{ active: boolean }>`
   padding: 1rem;
+  cursor: pointer;
+  ${({ active, theme }) => {
+    if (active) {
+      return css`
+        color: ${theme.colors.blue};
+        border-bottom: 2px solid ${theme.colors.blue};
+      `;
+    } else {
+      return css`
+        color: ${theme.colors.black};
+        border-bottom: 2px solid transparent;
+      `;
+    }
+  }}
 `;
