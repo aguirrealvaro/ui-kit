38a7dc3aff09ba127f443041ba51dac3ccd8b124
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2022-09-22 16:56:32 -0300

    use palette con colormode

--- src/ColorMode.tsx
+++ src/ColorMode.tsx
@@ -1,13 +1,19 @@
 import { FunctionComponent } from "react";
+import styled from "styled-components";
+import { Button } from "./components";
 import { useTheme } from "./hooks";
 
 export const ColorMode: FunctionComponent = () => {
   const { colorMode, toggleColorMode } = useTheme();
 
   return (
-    <div>
-      <span>color mode: {colorMode}</span>
-      <button onClick={toggleColorMode}>Toggle color!</button>
-    </div>
+    <Container>
+      <div>color mode: {colorMode}</div>
+      <Button onClick={toggleColorMode}>Toggle color!</Button>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  background-color: ${({ theme }) => theme.palette.grey[2]};
+`;
