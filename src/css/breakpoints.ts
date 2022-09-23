cc71c75213f5b05403f7de888282b520d56125ea
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2022-09-23 13:12:13 -0300

    Improving theme types

---
+++ src/css/breakpoints.ts
@@ -0,0 +1,8 @@
+type BreakpointSize = "xs" | "sm" | "md" | "lg";
+
+export const breakpoints: Record<BreakpointSize, string> = {
+  xs: "480px",
+  sm: "768px",
+  md: "992px",
+  lg: "1280px",
+};
--- src/css/theme.ts
+++ src/css/theme.ts
@@ -1,6 +1,7 @@
+import { breakpoints } from "./breakpoints";
 import { darkColors } from "./darkColors";
 import { lightColors } from "./lightColors";
-import { BreakpointSize, ColorModeType, ThemeType, WidthPx } from "./types";
+import { ColorModeType, ThemeType } from "./types";
 import { typography } from "./typography";
 
 // Asset colors: title, primary text, secondary text, disable, border, dividers, background, transparency
@@ -8,13 +9,6 @@ import { typography } from "./typography";
 // z index
 // box shadows
 // transitions
-
-const breakpoints: Record<BreakpointSize, WidthPx> = {
-  xs: "480px",
-  sm: "768px",
-  md: "992px",
-  lg: "1280px",
-};
 
 export const getTheme = (themeMode: ColorModeType): ThemeType => {
   const colors = themeMode === "light" ? lightColors : darkColors;
