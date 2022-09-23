ae9ad4248750e7214d2aab365ec167e62b07d065
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2022-09-22 22:11:37 -0300

    LocalStorage color mode

--- src/contexts/ThemeContext.tsx
+++ src/contexts/ThemeContext.tsx
@@ -1,5 +1,6 @@
 import { createContext, FunctionComponent, ReactNode, useState } from "react";
 import { ThemeProvider as StyledThemeProvider } from "styled-components";
+import { COLOR_MODE_KEY } from "@/constants";
 import { getTheme } from "@/css/theme";
 import { ColorModeType, ThemeType } from "@/css/types";
 
@@ -16,10 +17,14 @@ type ThemeProviderProps = {
 export const ThemeContext = createContext<ThemeContextType>({} as ThemeContextType);
 
 export const ThemeProvider: FunctionComponent<ThemeProviderProps> = ({ children }) => {
-  const [colorMode, setColorMode] = useState<ColorModeType>("light");
+  const [colorMode, setColorMode] = useState<ColorModeType>(() => {
+    return (localStorage.getItem(COLOR_MODE_KEY) || "light") as ColorModeType;
+  });
 
   const toggleColorMode = () => {
-    setColorMode(colorMode === "light" ? "dark" : "light");
+    const mode = colorMode === "light" ? "dark" : "light";
+    setColorMode(mode);
+    localStorage.setItem(COLOR_MODE_KEY, mode);
   };
 
   const theme = getTheme(colorMode);
