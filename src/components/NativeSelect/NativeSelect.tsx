c8b01d66722cc44a245443fbda190343fef588e0
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2022-09-27 20:55:26 -0300

    More select

--- src/components/NativeSelect/NativeSelect.tsx
+++ src/components/NativeSelect/NativeSelect.tsx
@@ -1,6 +1,9 @@
 import { FunctionComponent, SelectHTMLAttributes } from "react";
+import { ChevronDown } from "@styled-icons/boxicons-regular/ChevronDown";
 import styled from "styled-components";
+import { Icon } from "../Icon";
 import { NativeSelectFieldType } from "./NativeSelect.types";
+import { useTheme } from "@/hooks";
 
 type NativeSelectProps = {
   options: NativeSelectFieldType[];
@@ -9,7 +12,7 @@ type NativeSelectProps = {
 export const NativeSelect: FunctionComponent<
   NativeSelectProps & Omit<SelectHTMLAttributes<HTMLSelectElement>, "size">
 > = ({ value, onChange, options, placeholder }) => {
-  console.log(value);
+  const { theme } = useTheme();
 
   return (
     <Container>
@@ -25,42 +28,53 @@ export const NativeSelect: FunctionComponent<
           );
         })}
       </SelectContainer>
+      <SideContainer>
+        <ChevronWrapper active={false}>
+          <Icon icon={ChevronDown} color={theme.colors.grey.base} size={23} />
+        </ChevronWrapper>
+      </SideContainer>
     </Container>
   );
 };
 
 const Container = styled.div`
   position: relative;
-  display: inline-block;
-  font-size: 14px;
-  color: #888;
+  display: flex;
+  font-size: 16px;
+  color: ${({ theme }) => theme.colors.grey[9]};
   width: 100%;
-  &::after {
-    content: "";
-    border-width: 5px;
-    border-style: solid;
-    border-color: transparent;
-    border-top-color: #222;
-    display: inline-block;
-    border-radius: 2px;
-    position: absolute;
-    right: 10px;
-    bottom: 10px;
-  }
+  border: 1px solid ${({ theme }) => theme.colors.grey[5]};
+  border-radius: ${({ theme }) => theme.borderRadius.sm};
 `;
 
 const SelectContainer = styled.select`
-  display: block;
-  font-size: 16px;
+  font-size: inherit;
   width: 100%;
-  min-height: 35px;
+  height: 40px;
   background-color: transparent;
-  border-radius: ${({ theme }) => theme.borderRadius.sm};
+  border: none;
   outline: none;
-  padding: 0 1rem;
   cursor: pointer;
+  padding: 0 1rem;
   appearance: none;
   -webkit-appearance: none;
   -moz-appearance: none;
   -ms-appearance: none;
+`;
+
+const SideContainer = styled.div`
+  position: absolute;
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  right: 1rem;
+  top: 50%;
+  transform: translateY(-50%);
+`;
+
+const ChevronWrapper = styled.div<{ active: boolean }>`
+  transform: ${({ active }) => `rotate(${active ? "-180" : 0}deg)`};
+  transition: transform ${({ theme }) => theme.transitions.normal}ms ease;
+  display: flex;
+  align-items: center;
 `;
