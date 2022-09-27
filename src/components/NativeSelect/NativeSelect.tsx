3123aafb6de6802a057c6ed5be03c4419aa3db98
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2022-09-27 18:59:09 -0300

    some styles

--- src/components/NativeSelect/NativeSelect.tsx
+++ src/components/NativeSelect/NativeSelect.tsx
@@ -1,4 +1,5 @@
 import { FunctionComponent, SelectHTMLAttributes } from "react";
+import styled from "styled-components";
 import { NativeSelectFieldType } from "./NativeSelect.types";
 
 type NativeSelectProps = {
@@ -8,10 +9,12 @@ type NativeSelectProps = {
 export const NativeSelect: FunctionComponent<
   NativeSelectProps & Omit<SelectHTMLAttributes<HTMLSelectElement>, "size">
 > = ({ value, onChange, options, placeholder }) => {
+  console.log(value);
+
   return (
     <div>
-      <select value={value} onChange={onChange}>
-        <option hidden selected>
+      <SelectContainer value={value} onChange={onChange}>
+        <option selected value={undefined}>
           {placeholder}
         </option>
         {options.map(({ label, value, disabled }) => {
@@ -21,7 +24,17 @@ export const NativeSelect: FunctionComponent<
             </option>
           );
         })}
-      </select>
+      </SelectContainer>
     </div>
   );
 };
+
+const SelectContainer = styled.select`
+  appearance: none;
+  outline: none;
+  border: none;
+  background: none;
+  border-radius: ${({ theme }) => theme.borderRadius.xs};
+  border: 1px solid ${({ theme }) => theme.colors.grey[5]};
+  padding: 1rem;
+`;
