bafbf4760cd281f486832164acd9bd18aa3a4c3d
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2022-09-29 02:11:29 -0300

    Breakpoints in rem

--- src/css/theme/breakpoints.ts
+++ src/css/theme/breakpoints.ts
@@ -1,11 +1,11 @@
 export type BreakpointSize = "xs" | "sm" | "md" | "lg" | "xl";
 
 export const breakpoints: Record<BreakpointSize, string> = {
-  xs: "480px",
-  sm: "768px",
-  md: "992px",
-  lg: "1280px",
-  xl: "1440px",
+  xs: "30em", //480px
+  sm: "48em", //768px
+  md: "62em", //992px
+  lg: "80em", //1280px
+  xl: "96em", //1440px
 };
 
 export const BREAKPOINTS_ORDER: BreakpointSize[] = ["xl", "lg", "md", "sm", "xs"];
