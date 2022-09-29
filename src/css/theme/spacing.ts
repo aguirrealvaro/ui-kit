899c37a98596507b35d9099d946fcc4bba1f4438
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2022-09-29 14:24:16 -0300

    Add spacing comment

--- src/css/theme/spacing.ts
+++ src/css/theme/spacing.ts
@@ -1,6 +1,8 @@
-export type Spacing = "px" | 0.5 | 1 | 1.5 | 2 | 2.5 | 3 | 3.5 | 4 | 5 | 6 | 7 | 8;
+export type Spacing = "px" | 0.5 | 1 | 1.5 | 2 | 2.5 | 3 | 3.5 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
 
 export type SpacingType = Record<Spacing, string>;
+
+// Mental model: If you need a spacing of 40px, divide it by 4. That'll give you 10. Then use it in your component.
 
 export const spacing: SpacingType = {
   px: "1px",
@@ -16,4 +18,6 @@ export const spacing: SpacingType = {
   6: "1.5rem",
   7: "1.75rem",
   8: "2rem",
+  9: "2.25rem",
+  10: "2.5rem",
 };
