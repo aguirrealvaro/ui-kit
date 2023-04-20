bbf768f6067dae9d4f648cd90442c8dcedf0c034
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2023-04-19 21:57:35 -0300

    decrease spinner xs

--- src/components/Spinner/Spinner.constants.ts
+++ src/components/Spinner/Spinner.constants.ts
@@ -1,8 +1,8 @@
 import { SpinnerSizeType } from "./Spinner.types";
 
 export const SPINER_SIZES: Record<SpinnerSizeType, number> = {
-  xs: 18,
-  sm: 22,
-  md: 26,
-  lg: 30,
+  xs: 16,
+  sm: 20,
+  md: 24,
+  lg: 28,
 };
