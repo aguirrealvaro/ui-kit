d5598b40156ab8b8bbc1947fa34f52571a2b79ea
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2023-04-21 13:22:17 -0300

    Add spacing

--- src/components/Separator/Separator.tsx
+++ src/components/Separator/Separator.tsx
@@ -1,18 +1,20 @@
 import { FunctionComponent } from "react";
 import styled from "styled-components";
 import { SeparatorOrientatorType } from "./Separator.types";
+import { Spacing } from "@/css/theme/spacing";
 
 type SeparatorProps = {
   orientation: SeparatorOrientatorType;
+  spacing: Spacing;
 };
 
-export const Separator: FunctionComponent<SeparatorProps> = ({ orientation }) => {
-  return <Line orientation={orientation} />;
+export const Separator: FunctionComponent<SeparatorProps> = ({ orientation, spacing = 4 }) => {
+  return <Line orientation={orientation} spacing={spacing} />;
 };
 
-const Line = styled.hr<{ orientation: SeparatorOrientatorType }>`
+const Line = styled.hr<{ orientation: SeparatorOrientatorType; spacing: Spacing }>`
   background-color: ${({ theme }) => theme.assets.border};
   border: none;
-  margin: ${({ theme }) => theme.spacing[4]} 0;
+  margin: ${({ theme, spacing }) => theme.spacing[spacing]} 0;
   height: 1px;
 `;
