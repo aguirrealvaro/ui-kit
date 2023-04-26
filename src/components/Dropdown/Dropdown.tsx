2e350114495fde7c3e3f6d83d9413033ea9a674c
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2023-04-26 01:29:00 -0300

    Simplify dropdown!

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -1,5 +1,4 @@
 import { FunctionComponent } from "react";
-import styled from "styled-components";
 import { Popover, PopoverProps } from "../Popover";
 
 type DropdownProps = Omit<PopoverProps, "position"> & {
@@ -8,24 +7,12 @@ type DropdownProps = Omit<PopoverProps, "position"> & {
 
 export const Dropdown: FunctionComponent<DropdownProps> = ({
   children,
-  content,
   position = "bottom",
   ...restProps
 }) => {
-  const popoverContent = (
-    <Content withTriggerWidth={restProps.withTriggerWidth || false}>{content}</Content>
-  );
-
   return (
-    <Popover content={popoverContent} position={position} popUpType="menu" {...restProps}>
+    <Popover position={position} popUpType="menu" {...restProps}>
       {children}
     </Popover>
   );
 };
-
-const Content = styled.div<{ withTriggerWidth: boolean }>`
-  background-color: ${({ theme }) => theme.assets.bgTertiary};
-  box-shadow: ${({ theme }) => theme.shadows.sm};
-  border-radius: ${({ theme }) => theme.borderRadius.xs};
-  padding: ${({ theme }) => theme.spacing[5]};
-`;
