146d11384182f850358c337c3ed2a538c087dd29
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2023-04-28 16:51:32 -0300

    Remove unused colors

--- src/css/theme/colors.ts
+++ src/css/theme/colors.ts
@@ -1,14 +1,4 @@
-type Colors =
-  | "grey"
-  | "red"
-  | "orange"
-  | "yellow"
-  | "green"
-  | "teal"
-  | "blue"
-  | "indigo"
-  | "purple"
-  | "pink";
+type Colors = "grey" | "red" | "yellow" | "green" | "blue";
 
 type Variants = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
 
@@ -44,18 +34,6 @@ export const colors: CollorsType = {
     9: "#9B1C1C",
     10: "#771D1D",
   },
-  orange: {
-    1: "#FFF8F1",
-    2: "#FEECDC",
-    3: "#FCD9BD",
-    4: "#FDBA8C",
-    5: "#FF8A4C",
-    6: "#FF5A1F",
-    7: "#D03801",
-    8: "#B43403",
-    9: "#8A2C0D",
-    10: "#771D1D",
-  },
   yellow: {
     1: "#FDFDEA",
     2: "#FDF6B2",
@@ -80,18 +58,6 @@ export const colors: CollorsType = {
     9: "#03543F",
     10: "#014737",
   },
-  teal: {
-    1: "#EDFAFA",
-    2: "#D5F5F6",
-    3: "#AFECEF",
-    4: "#7EDCE2",
-    5: "#16BDCA",
-    6: "#0694A2",
-    7: "#047481",
-    8: "#036672",
-    9: "#05505C",
-    10: "#014451",
-  },
   blue: {
     1: "#f0fbff",
     2: "#d9f3ff",
@@ -103,41 +69,5 @@ export const colors: CollorsType = {
     8: "#1460b3",
     9: "#08448c",
     10: "#052d66",
-  },
-  indigo: {
-    1: "#F0F5FF",
-    2: "#E5EDFF",
-    3: "#CDDBFE",
-    4: "#B4C6FC",
-    5: "#8DA2FB",
-    6: "#6875F5",
-    7: "#5850EC",
-    8: "#5145CD",
-    9: "#42389D",
-    10: "#362F78",
-  },
-  purple: {
-    1: "#F6F5FF",
-    2: "#EDEBFE",
-    3: "#DCD7FE",
-    4: "#CABFFD",
-    5: "#AC94FA",
-    6: "#9061F9",
-    7: "#7E3AF2",
-    8: "#6C2BD9",
-    9: "#5521B5",
-    10: "#4A1D96",
-  },
-  pink: {
-    1: "#FDF2F8",
-    2: "#FCE8F3",
-    3: "#FAD1E8",
-    4: "#F8B4D9",
-    5: "#F17EB8",
-    6: "#E74694",
-    7: "#D61F69",
-    8: "#BF125D",
-    9: "#99154B",
-    10: "#751A3D",
   },
 };
